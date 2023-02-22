4afea5899d8b1202d1330de07d018f5ba99ba830
jiheon788 <jiheon788@ajou.ac.kr>
2023-02-23 02:11:55 +0900

    feat: 로그인

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,7 +1,12 @@
+import { postSignIn } from '@/api/auth';
 import useInputs from '@/lib/hooks/useInputs';
 import useValidation from '@/lib/hooks/useValidation';
+import { useNavigate } from 'react-router-dom';
+import token from '@/lib/token';
+import { ACCESS_TOKEN_KEY } from '@/constants/token.contant';
 
 const SignInpage = () => {
+  const navigate = useNavigate();
   const [signInData, onChangeSignInData] = useInputs({
     email: '',
     password: '',
@@ -9,10 +14,23 @@ const SignInpage = () => {
 
   const [emailStatus, passwordStatus] = useValidation(signInData);
 
+  const onSignIn = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    postSignIn(signInData)
+      .then((res) => {
+        token.setToken(ACCESS_TOKEN_KEY, res.data.access_token);
+        navigate('/todo');
+        window.location.reload();
+      })
+      .catch((err) => {
+        alert(err.response.data.log || err.log);
+      });
+  };
+
   return (
     <div>
       <h1>SignIn</h1>
-      <form>
+      <form onSubmit={onSignIn}>
         <input
           type="text"
           placeholder="이메일을 입력해주세요"
