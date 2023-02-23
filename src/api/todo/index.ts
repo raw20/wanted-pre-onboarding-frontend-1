0b41a62aa765b72a9681d298dbf03a8fd73b53ac
정찬욱 <raw1250@gmail.com>
2023-02-24 01:00:59 +0900

    feat: todo api 구현

--- src/api/todo/index.ts
+++ src/api/todo/index.ts
@@ -1,5 +1,9 @@
 import apiClient from '@/api/apiClient';
-import { createTodoType } from '@/api/todo/types';
+import {
+  createTodoType,
+  deleteTodoType,
+  updateTodoType,
+} from '@/api/todo/types';
 
 export const createTodo = async (todo: createTodoType) => {
   return await apiClient({
@@ -14,5 +18,22 @@ export const getTodos = async () => {
   return await apiClient({
     method: 'get',
     url: '/todos',
+  });
+};
+
+export const updateTodo = async ({ todo, isCompleted }: updateTodoType) => {
+  return await apiClient({
+    method: 'put',
+    url: '/todos',
+    data: {
+      todo,
+      isCompleted,
+    },
+  });
+};
+export const deleteTodo = async (id: deleteTodoType) => {
+  return await apiClient({
+    method: 'delete',
+    url: `/todos/${id}`,
   });
 };
--- src/api/todo/types.ts
+++ src/api/todo/types.ts
@@ -1,3 +1,9 @@
 export interface createTodoType {
   todo: string;
 }
+export interface updateTodoType extends createTodoType {
+  isCompleted: boolean;
+}
+export interface deleteTodoType {
+  id: number;
+}
