32210e1dc32d47256cb910a3bf1c35f73288d2d6
정찬욱 <raw1250@gmail.com>
2023-02-23 13:40:52 +0900

    feat: 이메일 & 패스워드 유효성 검사 구현

---
+++ src/api/auth/signUpController.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError } from 'axios';
+import { Dispatch, SetStateAction } from 'react';
+
+const signUpController = (
+  email: string,
+  password: string,
+  api: string,
+  setFeedbackMessage: Dispatch<SetStateAction<string>>,
+) => {
+  axios
+    .post(
+      `${api}/auth/signup`,
+      {
+        email: email,
+        password: password,
+      },
+      {
+        headers: { 'Content-Type': 'application/json' },
+      },
+    )
+    .then((response) => {
+      alert('회원가입이 완료되었습니다.');
+      window.location.replace('/signin');
+      return response.data;
+    })
+    .catch((error) => {
+      if (error instanceof AxiosError) {
+        setFeedbackMessage(error.response?.data.message);
+      }
+    });
+};
+
+export default signUpController;
--- src/components/Input/SignUpInput.tsx
+++ src/components/Input/SignUpInput.tsx
@@ -1,4 +1,5 @@
 import useConfirm from '@/lib/hooks/useConfirm';
+import { emailRegex } from '@/lib/utils/regex';
 import React, { FormEvent, ChangeEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -11,15 +12,36 @@ const SignUpInput = () => {
   } = useConfirm();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordValidation, setPasswordValidation] = useState('');
+
+  const [feedbackMessage, setFeedbackMessage] = useState(' ');
 
   const siginUpSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(email);
   };
-
   const getEmail = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
+    if (!emailRegex.test(event.target.value)) setIsEmailConfirm(false);
+    else {
+      setIsEmailConfirm(true);
+    }
   };
+
+  const getPassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+    if (event.target.value.length < 8) setIsPasswordConfirm(false);
+    else if (event.target.value !== passwordValidation)
+      setIsPasswordConfirm(false);
+    else setIsPasswordConfirm(true);
+  };
+
+  const getPasswordConfirm = (event: ChangeEvent<HTMLInputElement>) => {
+    setPasswordValidation(event.target.value);
+    if (event.target.value.length < 8) setIsPasswordConfirm(false);
+    else if (event.target.value !== password) setIsPasswordConfirm(false);
+    else setIsPasswordConfirm(true);
+  };
+
   return (
     <div>
       <div className="signin-input-wrap">
@@ -30,14 +52,29 @@ const SignUpInput = () => {
           </div>
           <div>
             <label>비밀번호 : </label>
-            <input />
+            <input type="password" value={password} onChange={getPassword} />
           </div>
           <div>
             <label>비밀번호 확인 : </label>
-            <input />
+            <input
+              type="password"
+              value={passwordValidation}
+              onChange={getPasswordConfirm}
+            />
           </div>
 
-          <button data-testid="signup-button">회원가입</button>
+          <button
+            disabled={
+              isEmailConfirm &&
+              isPasswordConfirm &&
+              password === passwordValidation
+                ? false
+                : true
+            }
+            data-testid="signup-button"
+          >
+            회원가입
+          </button>
           <Link to="/signin">로그인하러 가기</Link>
         </form>
       </div>
