477439ff61cccef11b1fdb773a2799d9ce5d9887
정찬욱 <raw1250@gmail.com>
2023-02-23 11:46:35 +0900

    feat: router path 연결

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,20 @@
+import HomePage from '@pages/HomePage';
+import SignInPage from '@pages/SigninPage';
+import SignUpPage from '@pages/SignUpPage';
+import TodoPage from '@pages/TodoPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 const Router = () => {
-  return <div>Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/todo" element={<TodoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
