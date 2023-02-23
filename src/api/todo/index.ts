9217e3970caab334aabd8cfad420a67de9a3cef2
jhoon9494 <wjdgns9406@gmail.com>
2023-02-24 00:15:39 +0900

    style: api 코드 형식 통일 및 api type 관련 파일명 변경

--- src/api/todo/index.ts
+++ src/api/todo/index.ts
@@ -1,9 +1,19 @@
 import apiClient from '@/api/apiClient';
+import { createTodoType } from '@/api/todo/types';
 
 export const getTodo = async () => {
-  return await apiClient.get('todos');
+  return await apiClient({
+    method: 'get',
+    url: '/todos',
+  });
 };
 
-export const createTodo = async (todo: string) => {
-  return await apiClient.post('todos', { todo });
+export const createTodo = async (todo: createTodoType) => {
+  return await apiClient({
+    method: 'post',
+    url: '/todos',
+    data: {
+      todo,
+    },
+  });
 };
